f126a4181bf1a8fbb0c87d1db376e252ed5c44d9
nirmalrepo <w.v.nirmalfernando@gmail.com>
2023-06-24 11:41:41 +1200

    Adding a fix to date format

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,14 +1,16 @@
-import defaultImgUrl from '../assets/default.jpg'
+import defaultImgUrl from "../assets/default.jpg";
 export function formatDate(date) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options = { month: "long", day: "numeric", year: "numeric" };
   const formattedDate = new Date(date).toLocaleDateString(undefined, options);
   return formattedDate;
 }
 
 export function getThumbnail(posterPath) {
-  const posterSize = 'w200';
+  const posterSize = "w200";
   if (!posterPath) {
     return defaultImgUrl;
   }
-  return `${import.meta.env.VITE_APP_IMAGE_BASE_URL}/${posterSize}/${posterPath}`;
+  return `${
+    import.meta.env.VITE_APP_IMAGE_BASE_URL
+  }/${posterSize}/${posterPath}`;
 }
